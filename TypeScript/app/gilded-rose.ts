c1f2eb3ccdf28a8e99081272a142a380ddf1e3ed
Polus Fansare (Paul) <polus.fansare@hiteshi.com>
2023-09-15 12:35:38 +0530

    Fixed Code Further to complete the task requirements, Updated Test Cases

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,79 +18,30 @@ export class GildedRose {
 	}
 
 	updateQuality() {
-		return this.items.map((item, i) => {
-			if(item.name != 'Sulfuras, Hand of Ragnaros' && item.quality > 0)
+		return this.items.map((item) => {
+			if(item.name !== 'Sulfuras, Hand of Ragnaros')
 			{
-				if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert')
-					item.quality -= 1;
+				item.sellIn -= 1;
+				if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert' && item.quality > 0)
+					item.quality -= (item.name !== 'Conjured Mana Cake') ? 1 : 2;
 				else if (item.quality < 50) {
-					item.quality += 1;
-					if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+					item.quality++
+					if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
 						if (item.sellIn < 11 && item.quality < 50)
-							item.quality += 1
-	
+							item.quality++
 						if (item.sellIn < 6 && item.quality < 50)
-							item.quality += 1
+							item.quality++
 					}
 				}
 	
-				item.sellIn -= 1;
-	
-				if (item.sellIn < 0) {
+				if (item.sellIn < 1) {
 					if (item.name !== 'Aged Brie')
-						item.quality -= item.name !== 'Backstage passes to a TAFKAL80ETC concert' ? 1 : item.quality
+						item.quality -= (item.name !== 'Backstage passes to a TAFKAL80ETC concert' && item.quality > 0 && item.sellIn !== 0) ? 1 : item.quality
 					else if (item.quality < 50)
 						item.quality += 1
 				}
 			}
 			return item;
 		});
-		/* for (let i = 0; i < this.items.length; i++) {
-			if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-				if (this.items[i].quality > 0) {
-					if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-						this.items[i].quality = this.items[i].quality - 1
-					}
-				}
-			} else {
-				if (this.items[i].quality < 50) {
-					this.items[i].quality = this.items[i].quality + 1
-					if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-						if (this.items[i].sellIn < 11) {
-							if (this.items[i].quality < 50) {
-								this.items[i].quality = this.items[i].quality + 1
-							}
-						}
-						if (this.items[i].sellIn < 6) {
-							if (this.items[i].quality < 50) {
-								this.items[i].quality = this.items[i].quality + 1
-							}
-						}
-					}
-				}
-			}
-			if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-				this.items[i].sellIn = this.items[i].sellIn - 1;
-			}
-			if (this.items[i].sellIn < 0) {
-				if (this.items[i].name != 'Aged Brie') {
-					if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-						if (this.items[i].quality > 0) {
-							if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-								this.items[i].quality = this.items[i].quality - 1
-							}
-						}
-					} else {
-						this.items[i].quality = this.items[i].quality - this.items[i].quality
-					}
-				} else {
-					if (this.items[i].quality < 50) {
-						this.items[i].quality = this.items[i].quality + 1
-					}
-				}
-			}
-		}
-
-		return this.items; */
 	}
 }
--- TypeScript/test/vitest/gilded-rose.spec.ts
+++ TypeScript/test/vitest/gilded-rose.spec.ts
@@ -2,7 +2,17 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
   it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const gildedRose = new GildedRose([new Item('foo', 0, 0),
+    new Item("+5 Dexterity Vest", 10, 20), //
+    new Item("Aged Brie", 2, 0), //
+    new Item("Elixir of the Mongoose", 5, 7), //
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80), // 
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    // this conjured item does not work properly yet
+    new Item("Conjured Mana Cake", 3, 6)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
